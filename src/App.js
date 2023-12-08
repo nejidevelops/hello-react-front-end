0bf00bb5779befc7f2c272475e5d844e8b5fa697
Newton Ombese <newtonombese1@gmail.com>
2023-12-08 06:34:31 +0300

    Successful setup of the route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+import Greeting from './Greeting';
 import './App.css';
 
 function App() {
   return (
     <>
-    
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
     </>
   );
 }
